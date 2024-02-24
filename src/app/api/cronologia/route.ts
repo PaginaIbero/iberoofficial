d224161e9a68a91b1442efb3e44b64849fa35704
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-24 11:51:12 -0300

    fix: imports in backend

--- src/app/api/cronologia/route.ts
+++ src/app/api/cronologia/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
+import { prisma } from '@/lib/db'
 
 
 export async function GET(req: NextRequest, res: NextResponse) {
     const cronologia = await prisma.cronologia.findMany({
         orderBy: {
             id: 'desc'
-        }
-    
+        },
     })
     return NextResponse.json(cronologia, { status: 200 })
 }
