7cfcce3b51dc332d80df970372f96b7cbd0b64a1
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-24 03:33:24 -0300

    Make components folder and add Section

---
+++ src/components/Section.tsx
@@ -0,0 +1,27 @@
+import { ChevronDownIcon } from "@heroicons/react/24/solid"
+import { useState } from "react"
+
+
+export function Section({ title, children }: {
+    title: string,
+    children: React.ReactNode
+  }) {
+    const [open, setOpen] = useState(false)
+    return (
+      <div className='flex flex-col border my-2'>
+        <div
+          className='flex justify-between p-4 bg-blue-100 hover:bg-blue-200 cursor-pointer transition-colors'
+          onClick={() => setOpen(!open)}
+        >
+          <h2 className='font-semibold'>
+            {title}
+          </h2>
+          <ChevronDownIcon
+            className={`w-6 transition-transform transform ${open ? 'rotate-180' : ''}`} />
+        </div>
+        <div className={`px-4 bg-white transition-[max-height] transition-max-height duration-[2s] overflow-hidden ${open ? 'max-h-auto' : 'max-h-0'}`}>
+          {children}
+        </div>
+      </div>
+    )
+  }
