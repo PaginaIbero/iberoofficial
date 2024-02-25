ef35a242cd959c74710cdaa61a3f1aabcbb24067
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-25 16:29:12 -0300

    feat: trying to fix deployment

--- src/app/ui/Cronologia.tsx
+++ src/app/ui/Cronologia.tsx
@@ -1,9 +1,14 @@
 import { getBaseUrl } from "@/lib/baseUrl";
 import { cronologia } from "@/lib/types";
 
-export async function Cronologia() {
+export async function Cronologia() {   
+  let cronologia: cronologia[] = []
+  try {
       const response = await fetch(`${getBaseUrl()}/api/cronologia`, {next: { revalidate: 3600 } });
-      const cronologia: cronologia[] = await response.json()
+      cronologia = await response.json()
+  } catch (error) {
+      console.error(error)
+  }
     
     return (
     <>
