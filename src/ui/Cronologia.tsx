c18169268e668534fa8c4c3c6ae99549a876a978
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-25 21:50:02 -0300

    feat: change fetch to axios

--- src/ui/Cronologia.tsx
+++ src/ui/Cronologia.tsx
@@ -1,10 +1,13 @@
 import { getBaseUrl } from "@/lib/baseUrl";
 import { cronologia } from "@/lib/types";
+import axios from "axios";
 
 export async function Cronologia() {   
   // let cronologia: cronologia[] = []
   // try {
-      const cronologia: cronologia[] = await fetch(`${getBaseUrl()}/api/cronologia`, {next: { revalidate: 3600 } }).then((res) => res.json()).catch((error) => console.error(error));
+      // const cronologia: cronologia[] = await fetch(`${getBaseUrl()}/api/cronologia`, {next: { revalidate: 3600 } }).then((res) => res.json()).catch((error) => console.error(error));
+        const cronologia: cronologia[] = await axios.get(`${getBaseUrl()}/api/cronologia`).then((res) => res.data).catch((error) => console.error(error));
+        console.log(cronologia)
   // } catch (error) {
   //     console.error(error)
   // }
