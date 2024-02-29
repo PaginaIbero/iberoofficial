d84d0ef952a6e6dd4742ca53634c1971be06e977
BrunZo <ziger.bruno@gmail.com>
2024-02-28 23:43:33 -0300

    feat: added fetch cronologia by id

--- src/app/cronologia/[id]/page.tsx
+++ src/app/cronologia/[id]/page.tsx
@@ -1,9 +1,24 @@
-export default function Page({ params }: { params: { id: string } }) {
+import { trpc } from "@/app/_trpc/client";
+
+export default function Page({ params }: { 
+  params: { 
+    id: string 
+  } 
+}) {
+  console.log(params.id)
+  // esta línea me da error y no sé cómo resolver
+  /*const data = trpc.cronologia.getByID.useQuery({
+    id: params.id
+  })*/
   return (
     <div className="flex flex-col text-black">
       <h1 className='text-4xl font-semibold text-center'>
         {params.id}
       </h1>
+      <h2 className='text-2xl font-semibold'>
+        Información general
+      </h2>
+
     </div>
   )
 }
--- src/server/routers/cronologiaRouter.ts
+++ src/server/routers/cronologiaRouter.ts
@@ -1,7 +1,15 @@
+import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 import prisma from '@/lib/db'
 
 export const cronologiaRouter = router({
+  getByID: publicProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+    return await prisma.resultados.findFirst({
+      where: {
+        id: input.id
+      }
+    });
+  }),
   getAll: publicProcedure.query(async () => {
     return await prisma.cronologia.findMany({
       orderBy: {
