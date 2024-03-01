c5e2c9db709ce6254483987cce9895b3a1a09be3
BrunZo <ziger.bruno@gmail.com>
2024-02-29 23:28:56 -0300

    feat: second attempt of using recharts

--- src/app/cronologia/[id]/page.tsx
+++ src/app/cronologia/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { trpc } from "@/app/_trpc/client";
 import { Suspense } from "react";
-import { PieChart, Pie, Cell, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, Legend, Tooltip, ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Bar } from 'recharts';
 
 export default function Page({ params }: {
   params: {
@@ -19,7 +19,7 @@ export default function Page({ params }: {
           Loading...
         </div>
       }>
-        <ResultadosData id={Number(params.id)}/>
+        <ResultadosData id={Number(params.id)} />
       </Suspense>
     </div>
   )
@@ -29,14 +29,20 @@ export function ResultadosData({ id }: {
   id: number
 }) {
   const [data, dataQuery] = trpc.cronologia.getByID.useSuspenseQuery(id)
-  const data01 = [
-    { name: 'Oro', value: 5 },
-    { name: 'Plata', value: 11 },
-    { name: 'Bronce', value: 23 },
-    { name: 'Mención', value: 40 },
-    { name: 'Nada', value: 37 }
+  const dataCortes = [{
+    nada: data?.cortes[2],
+    bronce: (data?.cortes[1] || 0) - (data?.cortes[2] || 0),
+    plata: (data?.cortes[0] || 0) - (data?.cortes[1] || 0),
+    oro: 42 - (data?.cortes[0] || 0)
+  }]
+  const dataPremios = [
+    { name: 'Oro', value: data?.premios[0] },
+    { name: 'Plata', value: data?.premios[1] },
+    { name: 'Bronce', value: data?.premios[2] },
+    { name: 'Mención', value: data?.premios[3] },
+    { name: 'Nada', value: (data?.participantes || 0) - (data?.premios.reduce((a, b) => a + b, 0) || 0) }
   ];
-  const COLORS = ['#FF8042', '#0088FE', '#FFBB28', '#00C49F'];
+  const COLORS = ['#ffd000', '#c0c0c0', '#c74900', '#ff009d', '#009dff'];
   return (
     <>
       <div className='flex '>
@@ -57,19 +63,37 @@ export function ResultadosData({ id }: {
           <h3 className='text-xl text-center font-semibold'>
             Premios
           </h3>
-          <ResponsiveContainer width="50%" height={250}>
-            <PieChart width={400} height={400}>
+          <ResponsiveContainer width="100%" height={40}>
+            <BarChart
+              width={20}
+              height={300}
+              data={dataCortes}
+              layout='vertical'
+            >
+              <CartesianGrid horizontalPoints={[]} verticalCoordinatesGenerator={(props) => (
+                [...Array(43).keys()].map((i) => i * props.width / 42 )
+              )} />
+              <XAxis hide type='number' domain={[0, 42]}/>
+              <YAxis hide type='category'/>
+              <Bar dataKey="nada" stackId="a" fill="#009dff" barSize={20} />
+              <Bar dataKey="bronce" stackId="a" fill="#c74900" barSize={20} />
+              <Bar dataKey="plata" stackId="a" fill="#c0c0c0" barSize={20} />
+              <Bar dataKey="oro" stackId="a" fill="#ffd000" barSize={20} />
+            </BarChart>
+          </ResponsiveContainer>
+          <ResponsiveContainer width="100%" height={250}>
+            <PieChart width={600} height={600}>
               <Pie
                 dataKey="value"
                 isAnimationActive={false}
-                data={data01}
+                data={dataPremios}
                 cx="50%"
                 cy="50%"
                 outerRadius={80}
                 fill="#8884d8"
                 label
               >
-                {data01.map((entry, index) => (
+                {dataPremios.map((entry, index) => (
                   <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
               </Pie>
