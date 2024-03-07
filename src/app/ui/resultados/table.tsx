e85c030a624f34c6b853da8fcfe923ab1bd33b0d
BrunZo <ziger.bruno@gmail.com>
2024-03-07 15:26:50 -0300

    fix: table boilerplate reduced

--- src/app/ui/resultados/table.tsx
+++ src/app/ui/resultados/table.tsx
@@ -1,119 +1,47 @@
 'use client';
 
+import { useRouter } from "next/navigation";
 import { trpc } from "@/app/_trpc/client";
 import { cronologia, resultado } from "@/lib/types";
-import { useRouter } from "next/navigation";
-import { TableBodySkeleton } from "../skeletons";
-
-// Resultados individuales única vez
+import { TableBodySkeleton } from "@/app/ui/skeletons";
+import Table from "@/app/ui/table";
 
 export function IndividualesTable({ id }: {
   id: number
 }) {
   const { data, isLoading } = trpc.resultados.getByFecha.useQuery(Number(id))
   return (
-    <table className='text-center text-black table-fixed'>
-      <thead className='font-semibold'>
-        <tr className='bg-blue-200'>
-          <th className='w-[5%] p-1'>#</th>
-          <th className='w-[50%] p-1'>Concursante</th>
-          <th className='w-[20%] p-1'>País</th>
-          <th className='w-[3%] p-1'>P1</th>
-          <th className='w-[3%] p-1'>P2</th>
-          <th className='w-[3%] p-1'>P3</th>
-          <th className='w-[3%] p-1'>P4</th>
-          <th className='w-[3%] p-1'>P5</th>
-          <th className='w-[3%] p-1'>P6</th>
-          <th className='w-[10%] p-1'>Total</th>
-          <th className='w-[10%] p-1'>Premio</th>
-        </tr>
-      </thead>
-      <tbody>
-        {isLoading ? <TableBodySkeleton/> : <IndividualesTableBody content={data}/>}
-      </tbody>
-    </table>
-  );
-}
-
-function IndividualesTableBody({ content }: {
-  content: resultado[] | undefined
-}) {
-  const router = useRouter();
-  return (
-    <>
-      {content?.map((item: resultado, index: number) => (
-        <tr
-          key={item.id}
-          className={`${index % 2 === 0 ? 'bg-blue-0 hover:bg-blue-100' : 'bg-blue-50 hover:bg-blue-100'} transition-colors cursor-pointer`}
-          onClick={() => {}}
-        >
-          <td className='py-3'>{item.ranking}</td>
-          <td className='py-3'>{item.nombreCompleto}</td>
-          <td className='py-3'>{item.pais}</td>
-          <td className='py-3'>{item.P1}</td>
-          <td className='py-3'>{item.P2}</td>
-          <td className='py-3'>{item.P3}</td>
-          <td className='py-3'>{item.P4}</td>
-          <td className='py-3'>{item.P5}</td>
-          <td className='py-3'>{item.P6}</td>
-          <td className='py-3'>{item.P1 + item.P2 + item.P3 + item.P4 + item.P5 + item.P6}</td>
-          <td className='py-3'>{item.medalla}</td>
-        </tr>
-      ))}
-    </>
+    <Table
+      headers={['#', 'Concursante', 'País', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'Total', 'Premio']}
+      data={data ? data.map((item: resultado) => [
+        item.ranking.toString(),
+        item.nombreCompleto,
+        item.pais,
+        item.P1.toString(),
+        item.P2.toString(),
+        item.P3.toString(),
+        item.P4.toString(),
+        item.P5.toString(),
+        item.P6.toString(),
+        (item.P1 + item.P2 + item.P3 + item.P4 + item.P5 + item.P6).toString(),
+        item.medalla
+      ]) : []}
+      isLoading={isLoading}
+    />
   )
 }
-
-// Resultados por país única vez
 
 export function PorPaisTable({ id }: {
   id: number
 }) {
   const { data, isLoading } = trpc.resultados.getByFecha.useQuery(Number(id))
   return (
-    <table className='text-center text-black table-fixed'>
-      <thead className='font-semibold'>
-        <tr className='bg-blue-200'>
-          <th className='p-1'></th>
-          <th className='p-1' colSpan={3}>Equipo</th>
-          <th className='p-1'></th>
-          <th className='p-1'></th>
-          <th className='p-1'></th>
-          <th className='p-1'></th>
-          <th className='p-1'></th>
-          <th className='p-1'></th>
-          <th className='p-1'></th>
-          <th className='p-1'></th>
-          <th className='p-1' colSpan={4}>Premios</th>
-          <th className='p-1'></th>
-          <th className='p-1'></th>
-        </tr>
-        <tr className='bg-blue-200'>
-          <th className='w-1/2 p-1'>País</th>
-          <th className='p-1'>Todos</th>
-          <th className='p-1'>H</th>
-          <th className='p-1'>M</th>
-          <th className='p-1'>P1</th>
-          <th className='p-1'>P2</th>
-          <th className='p-1'>P3</th>
-          <th className='p-1'>P4</th>
-          <th className='p-1'>P5</th>
-          <th className='p-1'>P6</th>
-          <th className='p-1'>Total</th>
-          <th className='p-1'>Puesto</th>
-          <th className='p-1'>O</th>
-          <th className='p-1'>P</th>
-          <th className='p-1'>B</th>
-          <th className='p-1'>MH</th>
-          <th className='p-1'>Jefe</th>
-          <th className='p-1'>Tutor</th>
-        </tr>
-      </thead>
-      <tbody>
-        {isLoading ? <TableBodySkeleton/> : <div>To be implemented</div>}
-      </tbody>
-    </table>
-  );
+    <Table
+      headers={['#', 'País', 'T', 'H', 'M', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'Total', 'O', 'P', 'B', 'MH', 'Jefe', 'Tutor']}
+      data={data ? data.map((item: resultado) => []) : []}
+      isLoading={isLoading}
+    />
+  )
 }
 
 // Resultados acumulados por fecha
@@ -188,12 +116,12 @@ function AcumuladoAnoTableBody({ content }: {
           <td className='py-3 hidden lg:table-cell'>{item.premios ? item.premios[1] : 'n/a'}</td>
           <td className='py-3 hidden lg:table-cell'>{item.premios ? item.premios[2] : 'n/a'}</td>
           <td className='py-3 hidden lg:table-cell'>{item.premios ? item.premios[3] : 'n/a'}</td>
-          <td className='py-3 border-l-2 border-white'>0</td>
-          <td className='py-3'>0</td>
-          <td className='py-3'>0</td>
-          <td className='py-3'>0</td>
-          <td className='py-3'>0</td>
-          <td className='py-3'>0</td>
+          <td className='py-3 border-l-2 border-white'>0.0</td>
+          <td className='py-3'>0.0</td>
+          <td className='py-3'>0.0</td>
+          <td className='py-3'>0.0</td>
+          <td className='py-3'>0.0</td>
+          <td className='py-3'>0.0</td>
         </tr>
       ))}
     </>
