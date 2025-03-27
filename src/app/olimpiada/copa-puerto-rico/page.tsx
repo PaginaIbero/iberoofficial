417de3e397a153683656e8a5ae5aa4021402ec9e
Bruno Ziger <59898191+BrunZo@users.noreply.github.com>
2025-03-27 16:20:02 -0300

    fix: add Date constructor

--- src/app/olimpiada/copa-puerto-rico/page.tsx
+++ src/app/olimpiada/copa-puerto-rico/page.tsx
@@ -1,7 +1,7 @@
 import { Section } from "@/app/ui/section";
 
 export default function Page() {
-  const today = Date.now()
+  let today = new Date(Date.now())
   return (
     <main className="flex min-h-screen flex-col text-black">
       <h1 className='text-4xl font-semibold text-center'>Copa Puerto Rico</h1>
