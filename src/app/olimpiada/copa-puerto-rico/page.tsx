6912120e2e1ca56ee871ad65e6bfbbed263e4e7f
Bruno Ziger <59898191+BrunZo@users.noreply.github.com>
2025-03-27 16:16:50 -0300

    fix: removed new

--- src/app/olimpiada/copa-puerto-rico/page.tsx
+++ src/app/olimpiada/copa-puerto-rico/page.tsx
@@ -1,7 +1,7 @@
 import { Section } from "@/app/ui/section";
 
 export default function Page() {
-  const today = new Date.now()
+  const today = Date.now()
   return (
     <main className="flex min-h-screen flex-col text-black">
       <h1 className='text-4xl font-semibold text-center'>Copa Puerto Rico</h1>
