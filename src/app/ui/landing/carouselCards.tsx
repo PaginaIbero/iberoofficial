3f4ef8b5526fde4f2636e8510f63f6fe6aeab871
BrunZo <ziger.bruno@gmail.com>
2025-05-26 23:52:34 -0300

    feat: changed title typography

--- src/app/ui/landing/carouselCards.tsx
+++ src/app/ui/landing/carouselCards.tsx
@@ -18,7 +18,7 @@ export function YearCard({ id }: {
 }) {
   return (
     <div className='flex flex-col justify-center items-center bg-blue-50 hover:bg-blue-100 transition-colors h-40 md:w-1/4 p-6 rounded-3xl border border-blue-200 shadow-sm'>
-      <h1 className='text-4xl font-semibold text-blue-800 mb-3'>{id}</h1>
+      <h1 className='text-4xl font-semibold font-sans text-blue-500 mb-3'>{id}</h1>
       <Link
         className='text-gray-700 hover:text-blue-600 text-sm mb-1 transition-colors'
         href={`/resultados/${id}`}
@@ -37,9 +37,9 @@ export function YearCard({ id }: {
 
 export function VerMasCard() {
   return (
-    <div className='flex flex-col justify-center items-center bg-blue-100 hover:bg-blue-200 transition-colors h-40 md:w-1/4 p-6 rounded-3xl border border-blue-300 shadow-sm'>
+    <div className='flex flex-col justify-center items-center bg-blue-50 hover:bg-blue-100 transition-colors h-40 md:w-1/4 p-6 rounded-3xl border border-blue-300 shadow-sm'>
       <Link 
-        className='text-blue-600 hover:text-blue-800 text-lg font-medium transition-colors'
+        className='text-gray-700 hover:text-blue-600 hover:text-blue-800 text-lg transition-colors'
         href='/cronologia'
       >
         Ver más
