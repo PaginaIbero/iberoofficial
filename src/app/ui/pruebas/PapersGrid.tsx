21a5eab8c2936d7021d50dcfbfbae01f4ecbce07
BrunZo <ziger.bruno@gmail.com>
2025-05-29 18:40:54 -0300

    fix: solved responsive in table

--- src/app/ui/pruebas/PapersGrid.tsx
+++ src/app/ui/pruebas/PapersGrid.tsx
@@ -1,4 +1,5 @@
-import { Cronologia } from "@prisma/client"
+'use client';
+
 import clsx from "clsx"
 import Link from "next/link"
 
@@ -10,27 +11,28 @@ export type PapersProps = {
 }
 
 export default function PapersGrid({ papers }: { papers: PapersProps[] }) {
-  console.log(papers)
   return (
-    <div className='grid grid-cols-6 gap-2'>
+    <div className='md:grid md:grid-cols-6 md:gap-2 flex flex-col'>
       {papers?.map((paper, i) => (
         <>
-          <span className='col-span-2'>
+          <span className='md:col-span-2'>
             {paper.id} - {paper.ciudad}, {paper.pais}
           </span>
-          {['sp', 'pt', 'en', 'sl'].map((lang, j) => (
-            <div key={j}>
-              <Link
-                className={clsx({
-                  'hover:underline cursor-pointer text-blue-500': paper.available[j],
-                  'text-gray-400': !paper.available[j],
-                })}
-                href={`/pruebas/${paper.id}-${lang}.pdf`}
-              >
-                {lang === 'sp' ? 'Español' : lang === 'pt' ? 'Portugués' : lang === 'en' ? 'Inglés' : 'Shortlist'}
-              </Link>
-            </div>
-          ))}
+          <div className='flex flex-row gap-4 md:contents w-full'>
+            {['sp', 'pt', 'en', 'sl'].map((lang, j) => (
+              <div key={j} className='md:col-span-1'>
+                <Link
+                  className={clsx({
+                    'hover:underline cursor-pointer text-blue-500': paper.available[j],
+                    'text-gray-400': !paper.available[j],
+                  })}
+                  href={`/pruebas/${paper.id}-${lang}.pdf`}
+                >
+                  {lang === 'sp' ? 'Español' : lang === 'pt' ? 'Portugués' : lang === 'en' ? 'Inglés' : 'Shortlist'}
+                </Link>
+              </div>
+            ))}
+          </div>
         </>
       ))}
     </div>
