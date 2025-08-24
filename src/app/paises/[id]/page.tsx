a28a5ae6daaf735bda5ec07515ff2841f56241f3
BrunZo <ziger.bruno@gmail.com>
2025-08-24 18:30:38 -0300

    Remove warning: await params

--- src/app/paises/[id]/page.tsx
+++ src/app/paises/[id]/page.tsx
@@ -1,10 +1,13 @@
 import CountryDetailsCardGrid from "@/ui/paises/CountryDashboard";
 
-export default async function Page({ params }: {
-  params: {
-    id: string
+export default async function Page(
+  props: {
+    params: Promise<{
+      id: string
+    }>
   }
-}) {
+) {
+  const params = await props.params;
   return (
     <div className="flex flex-col text-black">
       <CountryDetailsCardGrid id={params.id} />
--- src/app/resultados/[id]/page.tsx
+++ src/app/resultados/[id]/page.tsx
@@ -1,10 +1,13 @@
 import IndividualResultsCardGrid from "@/ui/resultados/YearResultsDashboard";
 
-export default function Page({ params }: {
-  params: {
-    id: number
+export default async function Page(
+  props: {
+    params: Promise<{
+      id: number
+    }>
   }
-}) {
+) {
+  const params = await props.params;
   return (
     <div className="flex flex-col text-black">
       <IndividualResultsCardGrid id={Number(params.id)} />
